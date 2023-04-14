438008cdad67e8ab6fe893b1cdcc5d51283e6848
Isral Herrera <israelherrerae@gmail.com>
2023-04-14 01:18:25 -0500

    chore: setup configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,35 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ConfigModule } from '@nestjs/config';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CakesModule } from './cakes/cakes.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: +process.env.DB_PORT || 5432,
+      username: process.env.DB_POSTGRES_USER,
+      password: process.env.DB_POSTGRES_PASSWORD,
+      database: process.env.DB_POSTGRES_DB,
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    CakesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
